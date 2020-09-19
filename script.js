617d5f0ece3fcd6b93d2015baa14b2f75abbb1a5
Benjamin <bhatfield120@gmail.com>
2020-09-19 12:27:31 +1000

    changed larger if/else statements to mapped objects, also set some variables to be global

--- script.js
+++ script.js
@@ -1,3 +1,130 @@
+
+//------------------------------------------------------------------------
+// listen for any button activity
+//------------------------------------------------------------------------
+
+function buttonListener() {
+    expression = [];
+    num1 = 0;
+    operator = '';
+    num2 = 0;
+    
+    const buttons = document.querySelectorAll('#button');
+    // we add listeners for each button on calculator
+    buttons.forEach(button => button.addEventListener('click', () => {
+        //for simplicity we set the input value
+        input = button.textContent;
+        whatButton(button);
+
+        })
+    )
+   
+}
+//------------------------------------------------------------------------
+// Chooses the appropriate button function
+//------------------------------------------------------------------------
+
+function whatButton(button) {
+    const display = document.querySelector('.display');
+    const buttonsList = {
+        'clear': clearButton,
+        'number' : numberButton,
+        'operator' : operations,
+        'equalizer' : equalsButton,
+        'backspace' : backspaceButton
+    }
+    return buttonsList[button.className](display);
+}
+
+//------------------------------------------------------------------------
+// Individual Button Functions
+//------------------------------------------------------------------------
+function clearButton(display) {
+    display.textContent = ''
+        expression = [];
+        num1 = 0;
+        operator = '';
+        num2 = 0;
+
+}
+
+function numberButton(display) {
+    display.textContent+=input;
+    expression.push(input);
+
+}
+
+function operations(display) {
+    if (operator!='') {
+        multiOperations(display);
+    } else {
+        operatorFirstPress(display);
+    }
+}
+
+function multiOperations(display) {
+    display.textContent+=input;
+    num2 = Number(expression.join(''));
+    num1 = operate(num1, operator, num2)
+    operator = input;
+    expression = [];
+
+}
+
+function operatorFirstPress(display) {
+    display.textContent+=input;
+    operator = input;
+    if (num1 == 0) {
+        num1 = Number(expression.join(''));
+        expression = [];
+    }     
+    
+}
+
+function equalsButton(display) {
+    if (display.textContent != '') {
+        num2 = Number(expression.join(''));
+        display.textContent = `${operate(num1, operator, num2)}`;
+        num1 = Number(display.textContent);
+        operator = '';
+        num2 = 0;
+        expression = [];
+    }
+
+}
+
+function backspaceButton(display) {
+    if (operator != '' && expression.length > 0) {
+        expression.pop();
+        display.textContent = display.textContent.slice(0, (display.textContent.length - 1));
+    } else if (operator != '') {
+        operator = '';
+        display.textContent = display.textContent.slice(0, (display.textContent.length - 1));
+    } else if (num1>0) {
+        display.textContent = '';
+        num1 = 0
+    }else {
+        expression.pop();
+        display.textContent = expression.join('');
+        
+    }    
+    
+}
+
+//------------------------------------------------------------------------
+// Decides which operator to choose
+//------------------------------------------------------------------------
+
+function operate(num1, operator, num2) {
+    const mapping = {
+        '+': add,
+        '-': subtract,
+        'x': multiply,
+        '/': divide
+    }
+    return mapping[operator](num1, num2);
+}
+
 //------------------------------------------------------------------------
 // Individual Operation Functions
 //------------------------------------------------------------------------
@@ -18,135 +145,5 @@ function divide(num1, num2) {
     return num1/num2;
 }
 
-//------------------------------------------------------------------------
-// Decides which operator to choose
-//------------------------------------------------------------------------
-function operate(num1, operator, num2) {
-    let answer = 0;
-    switch(operator) {
-        case "+":
-            answer = add(num1, num2);
-            break
-        case "-":
-            answer = subtract(num1, num2);
-            break
-        case "x":
-            answer = multiply(num1, num2);
-            break
-        case "/":
-            answer = divide(num1, num2);
-            break
-        default:
-            console.log("error");
-    }
-    return answer;
-}
-//------------------------------------------------------------------------
-// Individual Button Functions
-//------------------------------------------------------------------------
-function clearButton(variables, display) {
-    display.textContent = ''
-        variables.expression = [];
-        variables.num1 = 0;
-        variables.operator = '';
-        variables.num2 = 0;
-
-}
-
-function numberButton(variables, display) {
-    display.textContent+=variables.input;
-    variables.expression.push(variables.input);
-
-}
-
-function multiOperations(variables, display) {
-    display.textContent+=variables.input;
-    variables.num2 = Number(variables.expression.join(''));
-    variables.num1 = operate(variables.num1, variables.operator, variables.num2)
-    variables.operator = variables.input;
-    variables.expression = [];
-
-}
-
-function operatorFirstPress(variables, display) {
-    display.textContent+=variables.input;
-    variables.operator = variables.input;
-    if (variables.num1 == 0) {
-        variables.num1 = Number(variables.expression.join(''));
-        variables.expression = [];
-    }     
-    
-}
-
-function equalsButton(variables, display) {
-    variables.num2 = Number(variables.expression.join(''));
-    display.textContent = `${operate(variables.num1, variables.operator, variables.num2)}`;
-    variables.num1 = Number(display.textContent);
-    variables.operator = '';
-    variables.num2 = 0;
-    variables.expression = [];
-    
-}
-
-function backspaceButton(variables, display) {
-    if (variables.operator != '' && variables.expression.length > 0) {
-        variables.expression.pop();
-        display.textContent = display.textContent.slice(0, (display.textContent.length - 1));
-    } else if (variables.operator != '') {
-        variables.operator = '';
-        display.textContent = display.textContent.slice(0, (display.textContent.length - 1));
-    } else {
-        variables.expression.pop();
-        display.textContent = variables.expression.join('');
-        
-    }    
-    
-}
-
-//------------------------------------------------------------------------
-// Chooses the appropriate button function
-//------------------------------------------------------------------------
-
-function whatButton(variables, button, display) {
-    if (variables.input === 'clr') {
-        clearButton(variables, display);
-    //if a number is pressed we display it and add it to the expression array    
-    } else if (button.classList.contains('number')) {
-        numberButton(variables, display);
-    //if an operator is pressed in a string of calculations, the earlier expression is saved as num1     
-    } else if (button.classList.contains('operator') && variables.operator!='') {
-        multiOperations(variables, display);
-    //if its the first instance of operator, add to display and add set num1 value    
-    }  else if (button.classList.contains('operator')) {
-        operatorFirstPress(variables, display);
-    //if equals button is pressed perform operation, return result to display    
-    } else if (button.classList.contains('equalizer')) {
-        equalsButton(variables, display);
-    //if backspace is pressed remove last item from display    
-    } else if (button.classList.contains('backspace')) {
-        backspaceButton(variables, display);
-    }
-    
-}
-
-//------------------------------------------------------------------------
-// listen for any button activity
-//------------------------------------------------------------------------
-
-function buttonListener() {
-    let variables = {expression : [], num1 : 0, operator : '', num2 : 0};
-    //display is the artifical calculator screen
-    const display = document.querySelector('.display');
-    const buttons = document.querySelectorAll('#button');
-    // we add listeners for each button on calculator
-    buttons.forEach(button => button.addEventListener('click', () => {
-        //for simplicity we set the input value
-        variables.input = button.textContent;
-        whatButton(variables, button, display);
-
-        })
-    )
-   
-}
 
 buttonListener();
